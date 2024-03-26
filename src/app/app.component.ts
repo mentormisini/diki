709907db9a1e5d538e8d6a7d97b3c00c083a1b6b
MISINI Mentor <misinimentor@gmail.com>
2024-03-26 13:14:37 +0100

    git adsf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {DesignerComponent} from "./view/designer/designer.component";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DesignerComponent],
+  imports: [RouterOutlet, DesignerComponent, RouterLink, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
